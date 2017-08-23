b8782ccc178d9d2bd3daa519439207e1e2322dff
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-08-23 17:25:02 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 
 const meInReact =
-	React.createElement('div', { className: me },
+	React.createElement('div', { className: "me" },
   	React.createElement('h1', {}, 'An Awesome Person'),
   	React.createElement('p', {}, 'Who is learning React'),
     React.createElement('br'),
